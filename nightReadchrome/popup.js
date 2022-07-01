497c3d40dcc8898f40891815eda4b5d242cf1c82
MSTGr8 <64365070+MSTGr8@users.noreply.github.com>
2022-07-01 13:41:18 +0530

    stable release v3

--- nightReadchrome/popup.js
+++ nightReadchrome/popup.js
@@ -1,12 +1,34 @@
 
 newFunction();
 function newFunction() {
-    document.getElementById("hi").addEventListener("click", ()=>{
-        chrome.tabs.executeScript(null,{
-            code:`
-            var cover = document.createElement("div");
+    async function getCurrentTab(){
+        let queryOptions = { active: true, currentWindow: true };
+        let [tab] = await chrome.tabs.query(queryOptions);
+        return tab;
+    }
+    function fhii(){
+        var cover = document.createElement("div");
             
-            let css = \`
+            let css = `
+                position: fixed;
+                pointer-events: none;
+                top: 0;
+                left: 0;
+                width: 100vw;
+                height: 100vh;
+                background-color: white;
+                mix-blend-mode: difference;
+                opacity: 0.85;
+                z-index: 1;
+                `
+            cover.setAttribute("style", css);
+            document.body.appendChild(cover);
+            
+    }
+    function fhi(){
+        var cover = document.createElement("div");
+            
+            let css =` 
                 position: fixed;
                 pointer-events: none;
                 top: 0;
@@ -16,10 +38,16 @@ function newFunction() {
                 background-color: white;
                 mix-blend-mode: difference;
                 z-index: 1;
-            \`
+                `
             cover.setAttribute("style", css);
             document.body.appendChild(cover);
-            `
+    }
+    document.getElementById("hi").addEventListener("click", async ()=>{
+        let tab = await getCurrentTab();
+
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            func: fhi
         });
     });
  /*   document.getElementById("hii").addEventListener("click", ()=>{
@@ -44,26 +72,11 @@ function newFunction() {
             `
         });
     });*/
-    document.getElementById("ii").addEventListener("click", ()=>{
-        chrome.tabs.executeScript(null,{
-            code:`
-            var cover = document.createElement("div");
-            
-            let css = \`
-                position: fixed;
-                pointer-events: none;
-                top: 0;
-                left: 0;
-                width: 100vw;
-                height: 100vh;
-                background-color: white;
-                mix-blend-mode: difference;
-                opacity: 0.85;
-                z-index: 1;
-            \`
-            cover.setAttribute("style", css);
-            document.body.appendChild(cover);
-            `
+    document.getElementById("ii").addEventListener("click", async()=>{
+        let tab=await getCurrentTab();
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            func: fhii
         });
     });
 }
